a6bc5f55b4e6bc4e4482daa2d3537517a8a0c0b6
msaldanaescobar-beep <m.saldana.escobar@gmail.com>
2026-01-20 03:41:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ import { Telegraf, Markup } from "telegraf";
 // ===============================
 // CONFIGURACIÓN
 // ===============================
-const BOT_TOKEN = "8360011868:TU_TOKEN_REAL_AQUI"; // token del bot
-const ADMIN_CHAT_ID = 8415598577:AAFgea3lcNN-OrQ1Ro7Jgv6Z4Ihs5IMJKdA; // TU chat id personal
+const BOT_TOKEN = 8415598577:AAFgea3lcNN-OrQ1Ro7Jgv6Z4Ihs5IMJKdA; // token del bot
+const ADMIN_CHAT_ID = 8360011868; // TU chat id personal (numero)
 
 const bot = new Telegraf(BOT_TOKEN);
 
